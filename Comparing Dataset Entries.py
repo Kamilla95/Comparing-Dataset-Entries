e464150e4c979773520b74d3de71c45d64ed623c
Kamilla <65569451+Kamilla95@users.noreply.github.com>
2022-06-17 19:19:53 +0300

    Add files via upload

---
+++ Comparing Dataset Entries.py
@@ -0,0 +1,63 @@
+id_1 = input()
+average_grade_1 = input()
+test_score_1 = int(input())
+
+id_2 = input()
+average_grade_2 = input()
+test_score_2 = int(input())
+
+no_duplicates = id_1 != id_2
+print('No duplicate entries: ')
+print(no_duplicates)
+
+same_average = average_grade_1 == average_grade_2
+print('Same average grade:')
+print(same_average)
+
+higher_score = test_score_1 > test_score_2
+print('id_1 has a higher score:')
+print(higher_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
